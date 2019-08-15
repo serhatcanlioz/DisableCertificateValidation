1937efc107dc1e299f074131fbc9fc8bc215220a
Serhat ÇANLIÖZ <serhatcanlioz@hotmail.com>
2019-08-15 09:57:31 +0300

    Add files via upload

---
+++ CallRestApiSecure/src/com/serhat/Deneme.java
@@ -0,0 +1,70 @@
+package com.serhat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.*;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+
+public class Deneme {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Deneme.disableCertificateValidation();
+		Deneme.MyGETRequest();
+	}
+	public static void disableCertificateValidation() {
+	    // Create a trust manager that does not validate certificate chains
+	    TrustManager[] trustAllCerts = new TrustManager[] { 
+	      new X509TrustManager() {
+	        public X509Certificate[] getAcceptedIssuers() { 
+	          return new X509Certificate[0]; 
+	        }
+	        public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+	        public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+	    }};
+
+	    // Ignore differences between given hostname and certificate hostname
+	    HostnameVerifier hv = new HostnameVerifier() {
+	      public boolean verify(String hostname, SSLSession session) { return true; }
+	    };
+
+	    // Install the all-trusting trust manager
+	    try {
+	      SSLContext sc = SSLContext.getInstance("SSL");
+	      sc.init(null, trustAllCerts, new SecureRandom());
+	      HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+	      HttpsURLConnection.setDefaultHostnameVerifier(hv);
+	    } catch (Exception e) {}
+	  }
+	
+	public static void MyGETRequest() throws IOException {
+	    URL urlForGetRequest = new URL("https://....");
+	    String readLine = null;
+	    HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
+	    conection.setRequestMethod("GET");
+	    //conection.setRequestProperty("userId", "a1bcdef"); // set userId its a sample here
+	    int responseCode = conection.getResponseCode();
+	    if (responseCode == HttpURLConnection.HTTP_OK) {
+	        BufferedReader in = new BufferedReader(
+	            new InputStreamReader(conection.getInputStream()));
+	        StringBuffer response = new StringBuffer();
+	        while ((readLine = in .readLine()) != null) {
+	            response.append(readLine);
+	        } in .close();
+	        // print result
+	        System.out.println("JSON String Result " + response.toString());
+	        //GetAndPost.POSTRequest(response.toString());
+	    } else {
+	        System.out.println("GET NOT WORKED");
+	    }
+	}
+	
+	
+}
